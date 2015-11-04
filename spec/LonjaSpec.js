bc9836756f8cab3aba8400bbb5385b7445f6ead2
Ismael Ferrer <ifolmedo@gmail.com>
2015-11-04 14:08:05 +0100

    red, does not raise exception if load does not exceed 200 kg

--- spec/LonjaSpec.js
+++ spec/LonjaSpec.js
@@ -1,7 +1,29 @@
-describe("Lonja", function() {
+describe("Load", function() {
 
-	it("TODO", function() {
-  	expect(1).toBe(2);
+	it("can not exceed 200 kg", function() {
+		var products = {
+			vieira: 50,
+			pulpo: 200,
+			centollo: 0
+		};
+		var exceptionThrown = false;
+
+		try {
+			var carga = createLoad(products);
+		} catch (err) {
+			exceptionThrown = true;
+		}
+
+		expect(exceptionThrown).toBe(true);
   });
 
+	it("does not raise exception if load does not exceed 200 kg", function() {
+		var products = {
+			vieira: 50,
+			pulpo: 100,
+			centollo: 0
+		};
+
+		var carga = createLoad(products);
+	});
 });
